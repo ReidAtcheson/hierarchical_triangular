fe5499f7aa6e583352ecb799f0bb32fa5ef28007
Reid Atcheson <reid.atcheson@gmail.com>
2022-08-10 16:44:23 -0500

    adding some unit tests

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #import python unit test framework
 import unittest
-import htri
+from htri import *
+import numpy as np
 
 
 
@@ -14,7 +15,8 @@ class TestHierarchicalTriangular(unittest.TestCase):
         htri=make_htri_rng_dense(0,m,minm,k,rng)
         L=htri_to_dense(htri,0,m,minm,k)
         out=eval_htri(htri,0,m,minm,L)
-        self.assertTrue( jnp.linalg.norm(out - jnp.eye(m)) < 1e-14 )
+        res = jnp.linalg.norm(out - jnp.eye(m)) 
+        self.assertTrue( res < 1e-11 )
     def test_hierarchical_triangular_trans(self):
         m=128
         minm=8
@@ -24,4 +26,9 @@ class TestHierarchicalTriangular(unittest.TestCase):
         htri=make_htri_rng_dense(0,m,minm,k,rng)
         L=htri_to_dense(htri,0,m,minm,k)
         out=eval_htri_trans(htri,0,m,minm,L.T)
-        self.assertTrue( jnp.linalg.norm(out - jnp.eye(m)) < 1e-14 )
+        self.assertTrue( jnp.linalg.norm(out - jnp.eye(m)) < 1e-11 )
+
+
+
+
+unittest.main()
